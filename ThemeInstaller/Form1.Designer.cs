b56e52352d19dce1f21a5fa5f9c65caca2049291
iUseYahoo <logicatedlzyt@gmail.com>
2022-04-27 23:26:20 +1000

    Added files from Visual Studio build

---
+++ ThemeInstaller/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ThemeInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monoFlat_ThemeContainer1 = new MonoFlat.MonoFlat_ThemeContainer();
+            this.monoFlat_Label1 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_TextBox2 = new MonoFlat.MonoFlat_TextBox();
+            this.monoFlat_Button3 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Button2 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_TextBox1 = new MonoFlat.MonoFlat_TextBox();
+            this.monoFlat_Button1 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_CheckBox1 = new MonoFlat.MonoFlat_CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_TextBox2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button3);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_TextBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_CheckBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.button2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.button1);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = false;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(614, 291);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "Discord Theme Installer";
+            // 
+            // monoFlat_Label1
+            // 
+            this.monoFlat_Label1.AutoSize = true;
+            this.monoFlat_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label1.Location = new System.Drawing.Point(121, 130);
+            this.monoFlat_Label1.Name = "monoFlat_Label1";
+            this.monoFlat_Label1.Size = new System.Drawing.Size(109, 15);
+            this.monoFlat_Label1.TabIndex = 8;
+            this.monoFlat_Label1.Text = "Your picked theme:";
+            // 
+            // monoFlat_TextBox2
+            // 
+            this.monoFlat_TextBox2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_TextBox2.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_TextBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.monoFlat_TextBox2.Image = null;
+            this.monoFlat_TextBox2.Location = new System.Drawing.Point(234, 118);
+            this.monoFlat_TextBox2.MaxLength = 32767;
+            this.monoFlat_TextBox2.Multiline = false;
+            this.monoFlat_TextBox2.Name = "monoFlat_TextBox2";
+            this.monoFlat_TextBox2.ReadOnly = true;
+            this.monoFlat_TextBox2.Size = new System.Drawing.Size(372, 41);
+            this.monoFlat_TextBox2.TabIndex = 7;
+            this.monoFlat_TextBox2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.monoFlat_TextBox2.UseSystemPasswordChar = false;
+            // 
+            // monoFlat_Button3
+            // 
+            this.monoFlat_Button3.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Button3.Image = null;
+            this.monoFlat_Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button3.Location = new System.Drawing.Point(234, 73);
+            this.monoFlat_Button3.Name = "monoFlat_Button3";
+            this.monoFlat_Button3.Size = new System.Drawing.Size(120, 26);
+            this.monoFlat_Button3.TabIndex = 6;
+            this.monoFlat_Button3.Text = "Find Theme";
+            this.monoFlat_Button3.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button3.Click += new System.EventHandler(this.monoFlat_Button3_Click);
+            // 
+            // monoFlat_Button2
+            // 
+            this.monoFlat_Button2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Button2.Image = null;
+            this.monoFlat_Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button2.Location = new System.Drawing.Point(486, 73);
+            this.monoFlat_Button2.Name = "monoFlat_Button2";
+            this.monoFlat_Button2.Size = new System.Drawing.Size(120, 26);
+            this.monoFlat_Button2.TabIndex = 5;
+            this.monoFlat_Button2.Text = "Clear Log";
+            this.monoFlat_Button2.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button2.Click += new System.EventHandler(this.monoFlat_Button2_Click);
+            // 
+            // monoFlat_TextBox1
+            // 
+            this.monoFlat_TextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_TextBox1.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_TextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.monoFlat_TextBox1.Image = null;
+            this.monoFlat_TextBox1.Location = new System.Drawing.Point(13, 165);
+            this.monoFlat_TextBox1.MaxLength = 32767;
+            this.monoFlat_TextBox1.Multiline = true;
+            this.monoFlat_TextBox1.Name = "monoFlat_TextBox1";
+            this.monoFlat_TextBox1.ReadOnly = true;
+            this.monoFlat_TextBox1.Size = new System.Drawing.Size(593, 114);
+            this.monoFlat_TextBox1.TabIndex = 4;
+            this.monoFlat_TextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.monoFlat_TextBox1.UseSystemPasswordChar = false;
+            // 
+            // monoFlat_Button1
+            // 
+            this.monoFlat_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_Button1.Image = null;
+            this.monoFlat_Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button1.Location = new System.Drawing.Point(360, 73);
+            this.monoFlat_Button1.Name = "monoFlat_Button1";
+            this.monoFlat_Button1.Size = new System.Drawing.Size(120, 26);
+            this.monoFlat_Button1.TabIndex = 3;
+            this.monoFlat_Button1.Text = "Install Theme";
+            this.monoFlat_Button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button1.Click += new System.EventHandler(this.monoFlat_Button1_Click);
+            // 
+            // monoFlat_CheckBox1
+            // 
+            this.monoFlat_CheckBox1.Checked = false;
+            this.monoFlat_CheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monoFlat_CheckBox1.Location = new System.Drawing.Point(12, 73);
+            this.monoFlat_CheckBox1.Name = "monoFlat_CheckBox1";
+            this.monoFlat_CheckBox1.Size = new System.Drawing.Size(167, 16);
+            this.monoFlat_CheckBox1.TabIndex = 2;
+            this.monoFlat_CheckBox1.Text = "Discord Canary";
+            this.monoFlat_CheckBox1.CheckedChanged += new MonoFlat.MonoFlat_CheckBox.CheckedChangedEventHandler(this.monoFlat_CheckBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(546, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(579, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 291);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Discord Theme Installer";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.monoFlat_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private Button button2;
+        private Button button1;
+        private MonoFlat.MonoFlat_CheckBox monoFlat_CheckBox1;
+        private OpenFileDialog openFileDialog1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button2;
+        private MonoFlat.MonoFlat_TextBox monoFlat_TextBox1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button3;
+        private MonoFlat.MonoFlat_Label monoFlat_Label1;
+        private MonoFlat.MonoFlat_TextBox monoFlat_TextBox2;
+    }
+}
---
+++ ThemeInstaller/Form1.cs
@@ -0,0 +1,88 @@
+namespace ThemeInstaller
+{
+    public partial class Form1 : Form
+    {
+
+
+        public void addlog(String message)
+        {
+            var logbox = monoFlat_TextBox1;
+
+            logbox.Text += message + Environment.NewLine;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void monoFlat_Button2_Click(object sender, EventArgs e)
+        {
+            monoFlat_TextBox1.Text = "";
+            addlog("Log was cleared.");
+        }
+
+        private void monoFlat_Button1_Click(object sender, EventArgs e)
+        {
+            if (monoFlat_CheckBox1.Checked == false)
+            {
+                addlog("[-] No discord version was selected.");
+            }
+
+            if (monoFlat_TextBox2.Text.Length > 1)
+            {
+                var username = System.Environment.UserName.ToString();
+                var themefilename = monoFlat_TextBox2.Text;
+                var discordpath = "C:\\Users\\" + username + "\\AppData\\Roaming\\BetterDiscord\\themes\\";
+
+                string sourceFile = System.IO.Path.Combine(themefilename);
+
+                // trying to remove all words except for the <filename>.css part.
+                var rawname = Path.GetFileName(themefilename);
+                // destfile which takes the discord path and the rawname which has stripped the dir path except for the filename.css
+                string destFile = System.IO.Path.Combine(discordpath, rawname);
+
+                System.IO.Directory.CreateDirectory(discordpath);
+                //copy file (filename.css) to the discord theme folder
+                System.IO.File.Copy(sourceFile, destFile, true);
+
+            } else
+            {
+                addlog("[-] No theme was located.");
+            }
+        }
+
+        private void monoFlat_CheckBox1_CheckedChanged(object sender)
+        {
+
+        }
+
+        private void monoFlat_Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Css Files (*.css)|*.css|All Files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                addlog("[+] Picked theme: " + ofd.FileName.ToString() + ".");
+                monoFlat_TextBox2.Text = ofd.FileName.ToString();
+            } else
+            {
+                addlog("[-] Aborted theme picker.");
+            }
+        }
+    }
+}
